cf92c4646faca05ef907715f00f17b5a251bbf56
Lakshmi Pade <60639191+Lakshmipade@users.noreply.github.com>
2024-04-26 00:33:46 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,128 @@
+// let questions = [
+//     {
+//         numb: 1,
+//         question: "What does HTML stands for?",
+//         answer: "HyperText Markup Language",
+//         options: [
+//             "A. HighText Machine Language",
+//             "B. HyperText and links Markup Language",
+//             "C. HyperText Markup Language",
+//             "D. None of these"
+//         ]
+//     },
+
+//     {
+//         numb: 2,
+//         question: "What does CSS stands for?",
+//         answer: "Cascading Style Sheet",
+//         options: [
+//             "A. Cute Style Sheet",
+//             "B. Cascading Style Sheet",
+//             "C. Computer Style Size",
+//             "D. Computation Style Sheet"
+
+//         ]
+//     },
+
+//     {
+//         numb: 3,
+//         question: "<br/> What type of tag is this?",
+//         answer: "Break Tag",
+//         options: [
+//             "A. Break Tag",
+//             "B. A broken one",
+//             "C. An opening Tag",
+//             "D. All of these"
+//            ]
+//     },
+
+//     {
+//         numb: 4,
+//         question: "What is SQL stands for ?",
+//         answer: "Structured Query Language",
+//         options: [
+//             "A. Semi-Structured Query Lang",
+//             "B. Structured Query Language",
+//             "C. Both A. and B.",
+//             "D. Only B"
+//         ]
+//     },
+
+//     {
+//         numb: 5,
+//         question: "What is PHP stands for?",
+//         answer: "Hypertext Preprocessor",
+//         options: [
+//             "A. Pre-processor Hyper Plane",
+//             "B. Plane Hyper Plane",
+//             "C. Hypertext Preprocessor",
+//             "D. None of these above"
+
+//         ]
+//     },
+// ];
+
+let questions = [
+    {
+        numb: 1,
+        question: "What does HTML stands for?",
+        answer: "C. HyperText Markup Language",
+        options: [
+            "A. HighText Machine Language",
+            "B. HyperText and links Markup Language",
+            "C. HyperText Markup Language",
+            "D. None of these"
+        ]
+    },
+
+    {
+        numb: 2,
+        question: "What does CSS stands for?",
+        answer: "B. Cascading Style Sheet",
+        options: [
+            "A. Cute Style Sheet",
+            "B. Cascading Style Sheet",
+            "C. Computer Style Size",
+            "D. Computation Style Sheet"
+
+        ]
+    },
+
+    {
+        numb: 3,
+        question: "<br/> What type of tag is this?",
+        answer: "A. Break Tag",
+        options: [
+            "A. Break Tag",
+            "B. A broken one",
+            "C. An opening Tag",
+            "D. All of these"
+           ]
+    },
+
+    {
+        numb: 4,
+        question: "What is SQL stands for ?",
+        answer: "B. Structured Query Language",
+        options: [
+            "A. Semi-Structured Query Lang",
+            "B. Structured Query Language",
+            "C. Both A. and B.",
+            "D. Only B"
+        ]
+    },
+
+    {
+        numb: 5,
+        question: "What is PHP stands for?",
+        answer: "C. Hypertext Preprocessor",
+        options: [
+            "A. Pre-processor Hyper Plane",
+            "B. Plane Hyper Plane",
+            "C. Hypertext Preprocessor",
+            "D. None of these above"
+
+        ]
+    },
+];
+
